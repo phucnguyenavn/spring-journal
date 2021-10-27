f6a04fd2a9c598bd4fdbb88c9c048bc61d8842cc
phuc <nguyenphuc.vna@gmail.com>
2021-10-27 11:54:02 +0700

    initial project

---
+++ src/main/java/personal/springutility/SpringUtilityApplication.java
@@ -0,0 +1,13 @@
+package personal.springutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUtilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/springutility/SpringUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.springutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
